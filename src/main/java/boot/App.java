f16fdba9439ead69f4b68433df35d4d21e654cdb
Denis <rdenis9616@gmail.com>
2018-03-24 17:44:50 +0200

    Create Project
    Create maven project, add dependecies, make basic configurations.

---
+++ src/main/java/boot/App.java
@@ -0,0 +1,14 @@
+package boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Mindit on 07-Jul-17.
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class,args);
+    }
+}
---
+++ src/main/java/boot/config/PersistenceConfiguration.java
@@ -0,0 +1,51 @@
+package boot.config;
+
+import org.springframework.boot.autoconfigure.flyway.FlywayDataSource;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by Mindit on 06-Jul-17.
+ */
+@Configuration
+@ComponentScan(basePackages = {"boot"})
+public class PersistenceConfiguration extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public ViewResolver getViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("templates/");
+        //resolver.setSuffix(".html");
+        return resolver;
+    }
+
+//    @Bean
+//    @ConfigurationProperties(prefix="spring.datasource")
+//    @Primary
+//    public DataSource dataSource(){
+//        return DataSourceBuilder.create().build();
+//    }
+//
+//    @Bean
+//    @ConfigurationProperties(prefix="datasource.flyway")
+//    @FlywayDataSource
+//    public DataSource flywayDataSource(){
+//        return DataSourceBuilder.create().build();
+//    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer)
+    {
+        configurer.enable();
+    }
+}
