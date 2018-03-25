0b2b20d3413a09e58d5180f2668e280a1301e3ff
Denis <rdenis9616@gmail.com>
2018-03-25 16:28:57 +0300

    Add basic models
    Create class for Diseases and Symptoms tables

---
+++ src/main/java/boot/model/Symptom.java
@@ -0,0 +1,27 @@
+package boot.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Symptoms")
+public class Symptom {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ID")
+    private int id;
+    @Column(name = "NAME")
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
