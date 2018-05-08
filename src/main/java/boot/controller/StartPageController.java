450d0b93f1c7ce10f9fdd6f3c1f632f2fae1eeb7
Radu Denis Florian <rdenis9616@gmail.com>
2018-05-08 20:24:42 +0300

    Fixed login/register validation

--- src/main/java/boot/controller/StartPageController.java
+++ src/main/java/boot/controller/StartPageController.java
@@ -2,11 +2,14 @@ package boot.controller;
 
 import boot.model.User;
 import boot.service.UserService;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -23,19 +26,22 @@ public class StartPageController {
         return "StartPage";
     }
 
-    @RequestMapping(value = "loginValidation", method = RequestMethod.POST, produces = "application/json")
+    @RequestMapping(value = "loginValidation", method = RequestMethod.POST)
+    @ResponseBody
     public int validateLogin(@ModelAttribute("username") String username,
-                              @ModelAttribute("password") String password)
+                              @ModelAttribute("password") String password,
+                             Model model)
     {
         User user = userService.getByUsernameAndPassword(username,password);
         if(user != null)
         {
-            return HttpServletResponse.SC_ACCEPTED;
+            return HttpServletResponse.SC_OK;
         }
         return HttpServletResponse.SC_FORBIDDEN;
     }
 
     @RequestMapping(value = "registerValidation", method = RequestMethod.POST)
+    @ResponseBody
     public  int validateRegister(@ModelAttribute("username") String username,
                                  @ModelAttribute("password") String password,
                                  @ModelAttribute("email") String email)
@@ -49,7 +55,7 @@ public class StartPageController {
             return HttpServletResponse.SC_FORBIDDEN;
 
         userService.Add(user);
-        return HttpServletResponse.SC_ACCEPTED;
+        return HttpServletResponse.SC_OK;
     }
 
 }
